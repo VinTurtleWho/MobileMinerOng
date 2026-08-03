9dace146133f1a72d0831df6a0e92a33c1cc3e46
VinTurtleWho <macro@example.com>
2026-08-04 02:08:59 +0530

    fix: add missing Vec3 import in AStarPathfinder

--- src/main/java/com/mobileminerong/planning/pathfinding/AStarPathfinder.java
+++ src/main/java/com/mobileminerong/planning/pathfinding/AStarPathfinder.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.*;
 
